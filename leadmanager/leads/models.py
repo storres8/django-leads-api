fa9fd0c7dfd0444023ecd299d5f19feac230da13
storres8 <stevetorres7562@gmail.com>
2019-04-17 12:17:57 -0400

    created serializer.

--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class Lead(models.Model):
     name = models.CharField(max_length=256)
+    email = models.EmailField(max_length=256, unique=True)
+    message = models.CharField(max_length=600, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
