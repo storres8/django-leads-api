ce3cbbc6c1f60faed65b10d1dc9c3c15ce01e2d2
storres8 <stevetorres7562@gmail.com>
2019-04-18 12:45:58 -0400

    set up dashboard components

---
+++ leadmanager/frontend/src/components/leads /Form.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Form extends Component {
+  render() {
+    return (
+      <div>
+        <h1>add form for leads</h1>
+      </div>
+    );
+  }
+}
---
+++ leadmanager/frontend/src/components/leads /Leads.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Leads extends Component {
+  render() {
+    return (
+      <div>
+        <h1>add leads</h1>
+      </div>
+    );
+  }
+}
