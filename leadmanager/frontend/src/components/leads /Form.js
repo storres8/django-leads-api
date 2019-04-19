f8ac9325a748d13439cd6ffd4431c72fb5cd4fdd
storres8 <stevetorres7562@gmail.com>
2019-04-19 12:15:07 -0400

    created form to add lead. working.

--- leadmanager/frontend/src/components/leads /Form.js
+++ leadmanager/frontend/src/components/leads /Form.js
@@ -1,10 +1,65 @@
 import React, { Component } from "react";
 
 export default class Form extends Component {
+  state = {
+    name: "",
+    email: "",
+    message: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
   render() {
+    const { name, email, message } = this.state;
     return (
-      <div>
-        <h1>add form for leads</h1>
+      <div className="card card-body mt-4 mb-4">
+        <h2>Add Lead</h2>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <div className="form-group">
+            <label>Name</label>
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              onChange={e => this.handleChange(e)}
+              value={name}
+            />
+          </div>
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              className="form-control"
+              type="email"
+              name="email"
+              onChange={e => this.handleChange(e)}
+              value={email}
+            />
+          </div>
+          <div className="form-group">
+            <label>Message</label>
+            <textarea
+              className="form-control"
+              type="text"
+              name="message"
+              onChange={e => this.handleChange(e)}
+              value={message}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
