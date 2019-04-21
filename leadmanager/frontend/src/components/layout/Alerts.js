b2781a44595acae168cccd8e9396e6b8b9dd6188
storres8 <stevetorres7562@gmail.com>
2019-04-21 14:18:23 -0400

    finished errors reducer>

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -1,21 +1,38 @@
 import React, { Component, Fragment } from "react";
 import { withAlert } from "react-alert";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 class Alerts extends Component {
+  static propTypes = {
+    error: PropTypes.object.isRequired
+  };
+
+  componentDidUpdate(prevProps) {
+    const { error, alert } = this.props;
+
+    if (error !== prevProps.error) {
+      if (error.msg.name) {
+        alert.error(`Name: ${error.msg.name.join()}`);
+      }
+      if (error.msg.email) {
+        alert.error(`Email: ${error.msg.email.join()}`);
+      }
+      if (error.msg.message) {
+        alert.error(`Message: ${error.msg.message.join()}`);
+      }
+    }
+  }
+
   render() {
-    const { alert } = this.props;
-    return (
-      <Fragment>
-        <button
-          onClick={() => {
-            alert.show("Oh look, an alert!");
-          }}
-        >
-          Show Alert
-        </button>
-      </Fragment>
-    );
+    return <Fragment />;
   }
 }
 
-export default withAlert()(Alerts);
+const mapStateToProps = state => {
+  return {
+    error: state.errorsReducer
+  };
+};
+
+export default connect(mapStateToProps)(withAlert()(Alerts));
