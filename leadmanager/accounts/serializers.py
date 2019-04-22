3854292713a5144e2bb110e0d2aee9c4ef22d801
storres8 <stevetorres7562@gmail.com>
2019-04-21 21:26:19 -0400

    created a user serializer and register serializer for a user

---
+++ leadmanager/accounts/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+        def create(self, validated_data):
+            user = User.objects.create_user(validated_data['username'],
+                                            validated_data['email'],
+                                            validated_data['password'])
+            return user
