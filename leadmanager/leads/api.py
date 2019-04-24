75898712378e7c89e459df3c5c59adaffb6e3f02
storres8 <stevetorres7562@gmail.com>
2019-04-24 14:30:11 -0400

    made leads available only to those authenticated.

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -6,10 +6,12 @@ from .serializers import LeadSerializer
 class LeadViewSet(viewsets.ModelViewSet):
     serializer_class = LeadSerializer
 
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+
     def get_queryset(self):
         return self.request.user.leads.all
-
-    permission_classes = (permissions.IsAuthenticated,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Lead(models.Model):
-    owner = models.ForeignKey(
-        User, related_name="leads", on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=256)
     email = models.EmailField(max_length=256, unique=True)
     message = models.CharField(max_length=600, blank=True)
+    owner = models.ForeignKey(
+        User, related_name="leads", on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
