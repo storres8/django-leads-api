f7010b74ceb5a93f331ed057f82f22f558c02329
storres8 <stevetorres7562@gmail.com>
2019-04-25 12:39:39 -0400

    created protected routes on the backend.

--- leadmanager/accounts/api.py
+++ leadmanager/accounts/api.py
@@ -29,3 +29,13 @@ class LoginAPI(generics.GenericAPIView):
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)[1]
         })
+
+
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
