f891ee8f52b16bdbc8a140d2fb7e3fa857bbd022
storres8 <stevetorres7562@gmail.com>
2019-05-05 16:40:25 -0400

    changed dashboard layout.

--- leadmanager/frontend/src/components/leads /Dashboard.js
+++ leadmanager/frontend/src/components/leads /Dashboard.js
@@ -4,10 +4,14 @@ import React, { Component } from "react";
 
 class Dashboard extends Component {
   render() {
+    let cStyle = {
+      cursor: "pointer"
+    };
     return (
       // <div className="container mx-0">
-      <div className="row mt-3">
-        <div className="col-4">
+      <div>
+        {/* <div className="row mt-3"> */}
+        {/* <div className="col-4">
           <div className="container ml-2">
             <Form />
           </div>
@@ -15,6 +19,63 @@ class Dashboard extends Component {
         <div className="col-8">
           <div className="container mt-2">
             <Leads />
+          </div>
+        </div> */}
+        <div className="container">
+          <Leads />
+        </div>
+        <div className="text-center">
+          <button
+            data-toggle="modal"
+            data-target="#exampleModal"
+            style={cStyle}
+            className="btn btn-warning"
+            type="button"
+          >
+            Add a lead!
+            <i className="fas fa-user-plus ml-1" />
+          </button>
+        </div>
+
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabIndex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  Add a contact
+                </h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <Form />
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Save changes
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- leadmanager/frontend/src/components/leads /Leads.js
+++ leadmanager/frontend/src/components/leads /Leads.js
@@ -23,6 +23,13 @@ class Leads extends Component {
   };
 
   render() {
+    let tableStyles = {
+      maxHeight: "620px",
+      width: "inherit",
+      // border: "1px solid #ccc",
+      // overflow: "scroll",
+      overflowY: "scroll"
+    };
     let styles = {
       maxHeight: "63px",
       width: "380px",
@@ -30,6 +37,7 @@ class Leads extends Component {
       // overflow: "scroll",
       overflowY: "scroll"
     };
+
     return (
       <div>
         <div className="row">
@@ -37,55 +45,57 @@ class Leads extends Component {
             <h1 className="text-center">Current Leads</h1>
           </div>
         </div>
-        <table className="table table-hover">
-          <thead>
-            <tr className="table-success">
-              <th scope="col">ID</th>
-              <th scope="col" className="text-center">
-                Name
-              </th>
-              <th scope="col" className="text-center">
-                Email
-              </th>
-              <th scope="col" className="text-center">
-                Message
-              </th>
-              <th scope="col" />
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.leads.map((lead, index) => {
-              return (
-                <tr key={lead.id} className="table-light">
-                  <td>{index + 1}</td>
-                  <td className="text-center">{lead.name}</td>
-                  <td className="text-center">{lead.email}</td>
-                  <td className="py-2">
-                    <div className="py-0">
-                      <div style={styles}>{lead.message}</div>
-                    </div>
-                  </td>
-                  <td>
-                    <div className="text-right">
-                      <button
-                        className="btn btn-outline-danger btn-sm"
-                        onClick={() => this.handleDelete(lead.id)}
-                      >
-                        Delete
-                      </button>
-                      <button
-                        className="ml-2 btn btn-outline-info btn-sm"
-                        onClick={() => this.handleEdit()}
-                      >
-                        Edit
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <div className="table" style={tableStyles}>
+          <table className="table table-hover">
+            <thead>
+              <tr className="table-success">
+                <th scope="col">ID</th>
+                <th scope="col" className="text-center">
+                  Name
+                </th>
+                <th scope="col" className="text-center">
+                  Email
+                </th>
+                <th scope="col" className="text-center">
+                  Message
+                </th>
+                <th scope="col" />
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.leads.map((lead, index) => {
+                return (
+                  <tr key={lead.id} className="table-light">
+                    <td>{index + 1}</td>
+                    <td className="text-center">{lead.name}</td>
+                    <td className="text-center">{lead.email}</td>
+                    <td className="py-2">
+                      <div className="py-0">
+                        <div style={styles}>{lead.message}</div>
+                      </div>
+                    </td>
+                    <td>
+                      <div className="text-right">
+                        <button
+                          className="btn btn-outline-danger btn-sm"
+                          onClick={() => this.handleDelete(lead.id)}
+                        >
+                          Delete
+                        </button>
+                        <button
+                          className="ml-2 btn btn-outline-info btn-sm"
+                          onClick={() => this.handleEdit()}
+                        >
+                          Edit
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
